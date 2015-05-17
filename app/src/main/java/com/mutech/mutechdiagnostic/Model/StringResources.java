a914df175f988761c5f06355b9d9019632164cb4
aerobar <mbulotano@gmail.com>
2015-05-17 16:59:00 -0700

    Added String Resources

---
+++ app/src/main/java/com/mutech/mutechdiagnostic/Model/StringResources.java
@@ -0,0 +1,59 @@
+package com.mutech.mutechdiagnostic.Model;
+
+/**
+ * Created by Michael on 5/3/2015.
+ */
+public class StringResources {
+
+    public String page0heading() {
+        return "Where would you like to start?";
+    }
+
+    public String page0description() {
+        return "Write Description Here";
+    }
+
+    public String page0choice1() {
+        return "Sample Taken";
+    }
+
+    public String page0choice2() {
+        return "Sample Already Taken";
+    }
+
+    public String page1heading() {
+        return "HIV Tutorial: Picture Analysis";
+    }
+
+    public String page1description() {
+        return "Make sure your phone has a camera";
+    }
+
+    public String page1choice1() {
+        return "Next";
+    }
+
+    public String page2heading() {
+        return "Begin Picture Analysis";
+    }
+
+    public String page2description() {
+        return "Where would you like to start?";
+    }
+
+    public String page2choice1() {
+        return "Where would you like to start?";
+    }
+
+    public String page3heading() {
+        return "Where would you like to start?";
+    }
+
+    public String page3description() {
+        return "Where would you like to start?";
+    }
+
+    public String page3choice1() {
+        return "Where would you like to start?";
+    }
+}
